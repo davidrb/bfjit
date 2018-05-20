e743665a9b96ecb0808ea1acdfc5b9e2be52f3b0
David Badke <davidrbadke@gmail.com>
2018-05-20 01:30:58 -1000

    Refactor

--- bfjit/bfjit.cc
+++ bfjit/bfjit.cc
@@ -1,52 +1,44 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 #include <vector>
 #include <queue>
 #include <map>
-#include <fstream>
-#include <string.h>
-#include <cassert>
-#include <string>
 #include <stdexcept>
 
 using namespace std::string_literals;
 
-/**
- * rbx - data ptr
- * r12 - putchar
- * r13 - getchar
- */
+const auto prologue_x64 =
+  "\x55"               // push %rbp
+  "\x48\x89\xe5"       // mov %rsp, %rbp
+  "\x48\x89\xd3"       // mov %rdx, %rbx     // rbx - data ptr
+  "\x4c\x8b\x27"       // mov (%rdi), %r12   // r12 - putchar
+  "\x4c\x8b\x6f\x08"s; // mov 8(%rdi), %r13  // r13 - getchar
 
-const auto prologue_code =
-  "\x55"              // push %rbp
-  "\x48\x89\xe5"      // mov %rsp, %rbp
-  "\x4c\x8b\x27"      // mov (%rdi), %r12
-  "\x4c\x8b\x6f\x08"  // mov 8(%rdi), %r13
-  "\x48\x89\xd3"s;    // mov %rdx, %rbx
+const auto epilogue_x64 = "\xc9\xc3"s; // leave; ret
 
-const auto epilogue_code = "\xc9\xc3"s; // leave; ret
+const auto right_x64 = "\x48\x83\xc3\x01"s;  // add $1, %rbx
+const auto left_x64  = "\x48\x83\xeb\x01"s;  // sub $1, %rbx
 
-const auto right_code = "\x48\x83\xc3\x01"s;  // add $1, %rbx
-const auto left_code  = "\x48\x83\xeb\x01"s;  // sub $1, %rbx
+const auto plus_x64  = "\x80\x03\x01"s;  // addb $1, (%rbx)
+const auto minus_x64 = "\x80\x2b\x01"s;  // subb $1, (%rbx)
 
-const auto plus_code  = "\x80\x03\x01"s;  // addb $1, (%rbx)
-const auto minus_code = "\x80\x2b\x01"s;  // subb $1, (%rbx)
-
-const auto out_code = 
+const auto out_x64 = 
   "\x48\x0f\xb6\x3b"  // mov (%rbx), %rdi
   "\x41\xff\xd4"s;    // call *%r12
 
-const auto in_code = 
+const auto in_x64 = 
   "\x41\xff\xd5"      // call *%r13
   "\x88\x03"s;        // mov %rax, (%rbx)
 
-const auto call_loop_code =
+const auto call_loop_x64 =
   "\x80\x3b\x00"      // cmp $0, %(rbx)
   "\x74\x07"          // je . + 10
   "\xe8"              // call *n(%rip)
   "\xde\xad\xbe\xef"  // n
   "\xeb\xf4"s;        // jmp . - 11
 
-const auto loop_end_code = "\xc3"s; // ret
+const auto loop_end_x64 = "\xc3"s; // ret
 
 enum class token_t {
   program, loop, whitespace,
@@ -56,10 +48,10 @@ enum class token_t {
 };
 
 auto assembler = std::map<token_t, std::string> {
-  {token_t::plus, plus_code}, {token_t::minus, minus_code},
-  {token_t::left, left_code}, {token_t::right, right_code},
-  {token_t::in, in_code},     {token_t::out, out_code},
-  {token_t::loop, call_loop_code},
+  {token_t::plus, plus_x64}, {token_t::minus, minus_x64},
+  {token_t::left, left_x64}, {token_t::right, right_x64},
+  {token_t::in, in_x64},     {token_t::out, out_x64},
+  {token_t::loop, call_loop_x64},
   {token_t::whitespace, ""}
 };
 
@@ -70,7 +62,6 @@ auto char_to_token = std::map<char, token_t> {
 };
 
 auto find_closing_bracket(auto begin, auto end) {
-  assert(*begin == '[');
   int level = 0;
 
   for (auto it = begin; it != end; ++it) {
@@ -101,19 +92,67 @@ auto next_token(auto it, auto end) {
   return std::make_pair(token_t::whitespace, ++it);
 }
 
-auto gather_tokens(auto it, auto end) {
+auto tokenize(auto it, auto end) {
   auto tokens = std::vector<std::pair<token_t, const char*>>{};
 
   while (it != end) {
     auto [token, next] = next_token(it, end);
     tokens.emplace_back(std::make_pair(token, next));
-    assert(next <= end);
     it = next;
   }
   return tokens;
 }
 
-auto compile_loops(auto& output, auto it, auto const& tokens) {
+void compile_loop_call(auto& output, auto& offsets) {
+  output += call_loop_x64;
+
+  auto jump = offsets.front() - output.length() + 2;
+  offsets.pop();
+
+  *(int *)(&output[0] + output.length() - 6) = jump;
+}
+
+void compile_repeated(auto& output, auto token, auto times) {
+  while (times > 255) {
+    times -= 255;
+    output += assembler[token];
+    output[output.length()-1] = 0xFF;
+  }
+  if (times != 0) {
+    output += assembler[token];
+    output[output.length()-1] = times;
+  }
+}
+
+void compile_token(auto& output, auto& offsets, auto token, auto begin, auto end) {
+  if (token == token_t::loop) {
+    compile_loop_call(output, offsets);
+
+  } else if (token == token_t::plus || token == token_t::minus || token == token_t::left || token == token_t::right) {
+    auto times = std::distance(begin, end);
+    compile_repeated(output, token, times);
+
+  } else {
+    output += assembler[token];
+  }
+}
+
+void compile_tokens(auto& output, auto it, auto token, auto& tokens, auto& offsets) {
+  if (token == token_t::program)
+    output += prologue_x64;
+
+  for (auto [token, next] : tokens) {
+    compile_token(output, offsets, token, it, next);
+    it = next;
+  }
+
+  if (token == token_t::loop)
+    output += loop_end_x64;
+  else if (token == token_t::program)
+    output += epilogue_x64;
+}
+
+auto precompile_loops(auto& output, auto it, auto const& tokens) {
   auto offsets = std::queue<long>{};
 
   for (auto [token, next]: tokens) {
@@ -126,45 +165,18 @@ auto compile_loops(auto& output, auto it, auto const& tokens) {
   return offsets;
 }
 
-void emit_machine_code(auto& output, auto it, auto token, auto& tokens, auto& offsets) {
-  if (token == token_t::program)
-    output += prologue_code;
-
-  for (auto [token, next] : tokens) {
-    auto here = output.length();
-    output += assembler[token];
-
-    auto size = std::distance(it, next);
-    if (token == token_t::loop) {
-        assert(!offsets.empty());
-        auto jump = offsets.front() - here - 10;
-        offsets.pop();
-        output += call_loop_code;
-        memcpy(&output[here+6], (char *)&jump, 4);
-
-    } else if (token == token_t::plus || token == token_t::minus || token == token_t::left || token == token_t::right) {
-      memcpy(&output[output.length()-1], (char *)&size, 1);
-    }
-    it = next;
-  }
-  assert(offsets.empty());
-
-  if (token == token_t::loop)
-    output += loop_end_code;
-  else if (token == token_t::program)
-    output += epilogue_code;
-}
-
 auto compile(auto& output, const char* begin, const char* end, token_t token=token_t::program) -> long {
-  auto tokens = gather_tokens(begin, end);
-  auto offsets = compile_loops(output, begin, tokens);
+  auto tokens = tokenize(begin, end);
+  auto offsets = precompile_loops(output, begin, tokens);
   auto offset = output.length();
 
-  emit_machine_code(output, begin, token, tokens, offsets);
+  compile_tokens(output, begin, token, tokens, offsets);
+
   return offset;
 }
 
 auto bf_getchar() -> decltype(getchar()) {
+  fflush(nullptr);
   auto c = getchar();
   return c == EOF ? 0 : c;
 }
@@ -175,18 +187,16 @@ auto compile(auto const& source) {
 
   return [=]() {
     char data[1024*1024] = {0};
-    void *lib[] = {
-      (void *)&putchar,
-      (void *)&bf_getchar
-    };
+    void *lib[] = {(void *)&putchar, (void *)&bf_getchar};
+    auto bf_main = (void (*)(void *, void *, void *))(&program[0] + main_offset);
 
-    auto bf_main = (void (*)(void *, void *, void *))(program.c_str() + main_offset);
     bf_main(lib, nullptr, &data);
   };
 }
 
 int main(int argc, char *argv[]) try {
-  if (argc != 2) throw std::runtime_error{"usage: "s + argv[0] + " <file>"};
+  if (argc != 2)
+    throw std::runtime_error{"usage: "s + argv[0] + " <file>"};
 
   auto file = std::ifstream{argv[1]};
   if (!file) throw std::runtime_error{"error opening file"};
